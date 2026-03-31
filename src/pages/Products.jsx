97aa793e2e619af75b44207658297065d4f380e4
Bryan Zumba <zumbabryan69@gmail.com>
2026-03-30 22:24:32 -0500

    Pagina productos base

---
+++ src/pages/Products.jsx
@@ -0,0 +1,16 @@
+function Products() {
+  return (
+    <div>
+      <h1>Productos</h1>
+
+      <button>Agregar Producto</button>
+
+      <ul>
+        <li>Producto 1 - $10</li>
+        <li>Producto 2 - $20</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Products;
