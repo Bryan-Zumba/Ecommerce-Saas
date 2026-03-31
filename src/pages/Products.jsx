2e2c08ffd9068216a130d2434dc14ebed15af1b4
Bryan Zumba <zumbabryan69@gmail.com>
2026-03-30 23:25:55 -0500

    Pagina productos busqueda / tarjeta

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,14 +1,19 @@
 function Products() {
   return (
     <div>
-      <h1>Productos</h1>
+        <h1>Productos</h1>
 
-      <button>Agregar Producto</button>
+        <button>Agregar Producto</button>
 
-      <ul>
-        <li>Producto 1 - $10</li>
-        <li>Producto 2 - $20</li>
-      </ul>
+        <input type="search" placeholder="Buscar Producto"/>
+
+        <div style={{ display: "flex", gap: "10px", marginTop: "20px" }}>
+            <div style={{ border: "1px solid #ccc", padding: "10px" }}>
+            <h3>Coca Cola</h3>
+            <p>Precio: $15</p>
+            <p>Stock: 10</p>
+            </div>
+        </div>
     </div>
   );
 }
