eeff9f64f10b6a79864d5f597e051f389e783326
Bryan Robert Zumba Molleturo <zumbab@ecuador.h.local>
2026-04-02 16:42:37 -0500

    Mejora Tarjeta Tailwild

--- src/components/products/ProductCard.tsx
+++ src/components/products/ProductCard.tsx
@@ -6,10 +6,29 @@ type Props = {
 
 function ProductCard({data}:Props){
     return(
-      <div>
-        <img src={data.imagen} alt="" />
-        <h6>{data.categoria}</h6>
-        <h3>{data.nombre}</h3>
+      <div className="bg-white rounded-2xl">
+        <span className="bg-emerald-50 text-emerald-700 font-semibold text-xs rounded-full">{data.categoria}</span>
+        <img
+          src={data.imagen} 
+          alt={data.nombre} 
+          className="w-full h-50 object-cover rounded-xl mb-4"/>
+        
+        {/*Div para detalles de producto*/}
+        <div>
+
+          <h3 className="text-xl font-bold text-gray-900 mb-2">{data.nombre}</h3>
+
+          <p className="font-bold ">
+            <span className="">$</span>
+            <span>{data.precio.toFixed(2)}</span>
+          </p>
+
+          <p>
+            <span>Stock: </span>
+            <span className="font-semibold">{data.stock} unidades</span>
+          </p>
+        </div>
+
       </div>  
     );
 }
